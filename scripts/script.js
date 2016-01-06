910d6a6a40e088c923a3054700b3c70638014ea1
tikho <iatikho@gmail.com>
2016-01-06 15:13:16 +0300

    Changing font-weight on scroll

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,6 @@
+var font_weight_step = 10;
+var font_weight_counter = font_weight_step;
+
 $(function(){
     var lastScrollTop = 0, delta = 5;
     $(window).scroll(function(event){
@@ -6,17 +9,27 @@ $(function(){
        if(Math.abs(lastScrollTop - st) <= delta)
           return;
 
-      space = 0.02;
-      size = 15;
+      space = 0.015;
+      var lookingAtHuge = hugeScrolledIntoView();
        
        if (st > lastScrollTop){
            // downscroll code
-           console.log('scroll down');
            increaseLetterSpace(space);
+           if (lookingAtHuge){
+              font_weight_counter += font_weight_step;
+              if (font_weight_counter % 100 == 0){
+                increaseFontWeight(font_weight_counter);
+              }
+           }
        } else {
           // upscroll code
-          console.log('scroll up');
           decreaseLetterSpace(space);
+           if (lookingAtHuge){
+              font_weight_counter -= font_weight_step;
+              if (font_weight_counter % 100 == 0){
+                decreaseFontWeight(font_weight_counter);
+              }
+           }
        }
        lastScrollTop = st;
     });
@@ -54,4 +67,35 @@ function decreaseLetterSpace(space) {
   letter_spacing = getStyle(element, 'letter-spacing');
 
 element.style.letterSpacing = parseFloat(letter_spacing) - space + 'vw';
+}
+
+
+
+
+function increaseFontWeight(weight) {
+  var element = document.getElementById('font-size-changing'),
+  font_weight = getStyle(element, 'font-weight');
+
+  element.style.fontWeight = parseFloat(font_weight) + weight;
+}
+
+function decreaseFontWeight(weight) {
+  var element = document.getElementById('font-size-changing'),
+  font_weight = getStyle(element, 'font-weight');
+
+  element.style.fontWeight = parseFloat(font_weight) - weight;
+}
+
+function hugeScrolledIntoView()
+{
+    var $elem = $('#font-size-changing');
+    var $window = $(window);
+
+    var docViewTop = $window.scrollTop();
+    var docViewBottom = docViewTop + $window.height();
+
+    var elemTop = $elem.offset().top;
+    var elemBottom = elemTop + $elem.height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
 }
